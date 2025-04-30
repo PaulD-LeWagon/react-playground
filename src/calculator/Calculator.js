5b38042f08bc57776b5dfad8e66d6c139dccd7b1
Paul Devanney <pauldevanney92@gmail.com>
2025-04-30 19:57:12 +0100

    Remove keyboard input funtionality. Add dblClick copy to clipboard. Add auto scrolling the displays. Add global event to close the history menu. Refactor AppState class

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import AppState from "./AppState.class"
 import { arrayChunk as chunk } from "../app-utilities"
 import "./Calculator.css"
 
@@ -13,7 +14,6 @@ import "./Calculator.css"
 //         console.count(counterLabel)
 //         return ""
 //       })()}
-
 //       ${output}
 //     `
 //       // .replace(/\s+/g, " ")
@@ -21,7 +21,6 @@ import "./Calculator.css"
 //   )
 // }
 // debugThis("Test Run", 1, "Two", { three: 3 })
-
 // const counter = () => {
 //   console.count("Calculator Render")
 //   return ""
@@ -36,9 +35,7 @@ import "./Calculator.css"
 //     .replace(/\s+/g, " ")
 //     .trim()
 // )
-
 // console.log(console)
-
 // const nativeLogger = console.log
 // console.log = (...params) => {
 //   for (let i in params) {
@@ -46,131 +43,43 @@ import "./Calculator.css"
 //   }
 // }
 
-function injectDynamicVars(that, theObject = {}) {
-  for (const [k, v] of Object.entries(theObject)) {
-    that[k] = v
-  }
-
-  // let k = "value"
-  // let i = 0
-  // for (i = 1; i < 3; i++) {
-  //   eval("let " + k + i + "= " + i + ";")
-  // }
-  // // eslint-disable-next-line no-undef
-  // console.log("value1=" + value1)
-  // // eslint-disable-next-line no-undef
-  // console.log("value2=" + value2)
-  // // eslint-disable-next-line no-undef
-  // console.log("value3=" + value3)
-
-  return that
-}
-
 // ;((x) => {
 //   let i
-//   // Window method
-//   for (i = 1; i < 5; i++) {
-//     window["value" + i] = +i
-//   }
-//   // eslint-disable-next-line no-undef
-//   console.log("value1=" + value1)
-//   // eslint-disable-next-line no-undef
-//   console.log("value2=" + value2)
 //   // eslint-disable-next-line no-undef
 //   console.log("value3=" + value3)
-
 //   // Create a new Map to store dynamic variables
 //   let dynamicMap = new Map()
 //   dynamicMap.set("memory", "0")
 //   dynamicMap.set("displayValue", "0")
 //   dynamicMap.set("calcStack", [])
-
 //   console.log(dynamicMap.get("memory"))
 //   console.log(dynamicMap.get("displayValue"))
 //   console.log(dynamicMap.get("calcStack"))
-
-//   // Generate dynamic variable names and assign values
-//   // for (let i = 1; i <= 4; i++) {
-//   //     dynamicMap.set(`value${i}`, i);
-//   // }
-
-//   // Access and print the dynamically created variables
-//   // dynamicMap.forEach((value, key) => {
-//   //     console.log(`${key} = ${value}`);
-//   // });
-
 //   console.log(x)
 // })("Hello Closure World!")
 
 export default function Calculator({ domID = "my-calculator" }) {
   // App state vars
-  class AppState {
-    constructor(args = { memory: "0", displayValue: "0", calcStack: [] }) {
-      this.memory = args.memory
-      this.displayValue = args.displayValue
-      this.calcStack = args.calcStack
-    }
-    getCalculation() {
-      return `${this.calcStack.join("")}${this.displayValue}`
-    }
-    toString() {
-      return `m: (${this.memory}), dv: (${
-        this.displayValue
-      }) cs: [${this.calcStack.join(", ")}]`
-    }
-    newState(newState = {}) {
-      return Object.assign(new this.constructor(), this, newState)
-    }
-    *[Symbol.iterator]() {
-      yield* Object.values(this) // yield* Object.entries(this)
-    }
-  }
-
   const [appState, setAppState] = useState(new AppState())
   const [history, setHistory] = useState([])
   const [precision, setPrecision] = useState(3)
+  const primaryRef = useRef(null)
+  const memoryRef = useRef(null)
+  const calculationRef = useRef(null)
+  const displayRefs = [primaryRef, memoryRef, calculationRef]
 
-  // console.log(appState.toString())
-
-  // function Test(one) {
-  //   this.one = one
-  // }
-  // const test = injectDynamicVars(new Test("Testing"), appState)
-  // console.log(test)
-
-  // console.log(
-  //   appState.newState({
-  //     memory: 27,
-  //     displayValue: 9,
-  //     calcStack: ["19", "-", "76", "-"],
-  //   })
-  // )
-
-  // function Test(one, two, ...allTheRest) {
-  //   this.one = one
-  //   this.two = two
-  //   this.allTheRest = allTheRest
-  // }
-  // Test.prototype.toString = function (printToConsole = false) {
-  //   let retString = ""
-  //   for (var [key, val] of Object.entries(this)) {
-  //     retString += `${key}: ${val}\n`
-  //   }
-  //   if (printToConsole) {
-  //     console.log(retString)
-  //   }
-  //   return retString
-  // }
-  // Test.prototype[Symbol.iterator] = function* () {
-  //   // yield* Object.entries(this)
-  //   yield this.one
-  //   yield this.two
-  //   yield this.allTheRest
-  // }
-  // var test = new Test(1, 2, 3, 'four', 'five', 'six', 'seven')
-  // console.log('Test: ', ...test)
-
-  // console.log(history)
+  useEffect(() => {
+    // The displays to the immediately input value
+    displayRefs.forEach((dispRef, i) => {
+      const el = dispRef.current
+      if (el.scrollWidth > el.clientWidth) {
+        el.scroll({
+          left: el.scrollWidth,
+          behavior: "smooth",
+        })
+      }
+    })
+  })
 
   const stepBackInTimeTo = (thisIndex) => {
     if (history.length) {
@@ -203,15 +112,6 @@ export default function Calculator({ domID = "my-calculator" }) {
         displayValue: "",
       })
     })
-    // setAppState((as) => {
-    //   // Unpack the current state object
-    //   let [memory, displayValue, calcStack] = as
-    //   // Then just add theOperator and the displayValue to the calculation stack
-    //   calcStack = [...calcStack, displayValue, theOperator]
-    //   // And empty the display ready for the next term
-    //   displayValue = ""
-    //   return new AppState(memory, displayValue, calcStack)
-    // })
   }
 
   const doCommand = (theCommand) => {
@@ -232,12 +132,6 @@ export default function Calculator({ domID = "my-calculator" }) {
             displayValue: "0",
           })
         })
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   displayValue = "0"
-        //   calcStack = []
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
         break
 
       case "=":
@@ -251,11 +145,6 @@ export default function Calculator({ domID = "my-calculator" }) {
         // Do appState.memory recall - i.e. set appState.displayValue
         // to appState.memory variable.
         setAppState((as) => appState.newState({ displayValue: as.memory }))
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   displayValue = memory
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
         break
 
       case "M-":
@@ -263,30 +152,19 @@ export default function Calculator({ domID = "my-calculator" }) {
         addCurrentStateToHistory()
         // Do appState.memory delete - i.e. reset the appState.memory variable.
         setAppState((as) => appState.newState({ memory: "0" }))
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   memory = "0"
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
         break
 
       case "M+":
         // Add current state to history variable
         addCurrentStateToHistory()
-        // Do appState.memory add - i.e. add the appState.displayValue to appState.memory
-        // variable (add to existing value if exists)
+        // Do appState.memory add - i.e. add the appState.displayValue to
+        // appState.memory variable (add to existing value if exists)
         setAppState((as) => {
           return appState.newState({
             memory: String(Number(as.memory) + Number(as.displayValue)),
             displayValue: "0",
           })
         })
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   memory = String(Number(memory) + Number(displayValue))
-        //   displayValue = "0"
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
         break
 
       case "％":
@@ -301,14 +179,6 @@ export default function Calculator({ domID = "my-calculator" }) {
                 : String(Number(as.displayValue) / 100),
           })
         })
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   displayValue =
-        //     Number(as.displayValue) === 0
-        //       ? "0"
-        //       : String(Number(as.displayValue) / 100)
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
         break
 
       default:
@@ -349,14 +219,9 @@ export default function Calculator({ domID = "my-calculator" }) {
           break
 
         default:
-          throw Error(
-            `Calculation Error: t: (${total}), cs: (${appState.calcStack.join(
-              " "
-            )})`
-          )
+          throw Error(`Calculation Error: t: (${total}), state: (${appState})`)
       }
     }
-
     // Add current state to history variable
     addCurrentStateToHistory()
     setAppState((as) => {
@@ -365,12 +230,19 @@ export default function Calculator({ domID = "my-calculator" }) {
         calcStack: [],
       })
     })
-    // setAppState((as) => {
-    //   let [memory, displayValue, calcStack] = as
-    //   displayValue = String(Number(total.toFixed(precision)))
-    //   calcStack = []
-    //   return new AppState(memory, displayValue, calcStack)
-    // })
+  }
+
+  const copyToClipboard = async (el) => {
+    const copyText = el
+    try {
+      navigator.clipboard.writeText(copyText.innerText)
+      alert(`(${copyText.innerText}) copied to clipboard!`)
+    } catch (error) {
+      console.error(
+        `Error: Failed to copy, ${copyText.innerText}, to the clipboard.`,
+        error
+      )
+    }
   }
 
   const handleOperatorButtonClick = (event, theValue) => {
@@ -391,18 +263,8 @@ export default function Calculator({ domID = "my-calculator" }) {
     if (appState.displayValue.length === 1 && appState.displayValue === "0") {
       if (theValue === "∙") {
         setAppState((as) => appState.newState({ displayValue: "0." }))
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   displayValue = "0."
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
       } else {
         setAppState((as) => appState.newState({ displayValue: theValue }))
-        // setAppState((as) => {
-        //   let [memory, displayValue, calcStack] = as
-        //   displayValue = theValue
-        //   return new AppState(memory, displayValue, calcStack)
-        // })
       }
     } else {
       theValue = theValue !== "∙" ? theValue : "."
@@ -411,46 +273,23 @@ export default function Calculator({ domID = "my-calculator" }) {
           displayValue: `${as.displayValue}${theValue}`,
         })
       })
-      // setAppState((as) => {
-      //   let [memory, displayValue, calcStack] = as
-      //   displayValue = `${displayValue}${theValue}`
-      //   return new AppState(memory, displayValue, calcStack)
-      // })
     }
   }
 
-  const handleInputChange = (event, theValue) => {
-    console.debug("InputChange: ", theValue)
-    // const opsMatch = theValue.trim().match(/[*/+-^]|RT+$/)
-    // if (opsMatch) {
-    //   doOperation(opsMatch.shift())
-    // } else {
-    //   const comMatch = theValue.trim().match(/[C=%]|M\+|M-|M+$/)
-    //   if (comMatch) {
-    //     doCommand(comMatch.shift())
-    //   } else {
-    //     // Add current state to history variable
-    //     addCurrentStateToHistory()
-    //     // Strip any illegal characters and update app state
-    //     setAppState((as) => appState.newState({ displayValue: theValue.replace(/[^0-9.]+/, "") }))
-    //    // setAppState((as) => {
-    //    //   let [memory, displayValue, calcStack] = as
-    //    //   displayValue = theValue.replace(/[^0-9.]+/, "")
-    //    //   return new AppState(memory, displayValue, calcStack)
-    //    // })
-    //   }
-    // }
+  const handleDblClick = (event) => {
+    copyToClipboard(event.target)
   }
 
   const displayEventHandlers = {
-    handleInput: (e) => {
-      handleInputChange(e, e.target.innerHTML)
-    },
-    handleKeyDown: (e) => {
-      if (e.key === "Enter") {
-        doCommand("=")
-      }
-    },
+    // handleInput: (e) => {
+    //   handleInputChange(e, e.target.innerHTML)
+    // },
+    // handleKeyDown: (e) => {
+    //   if (e.key === "Enter") {
+    //     doCommand("=")
+    //   }
+    // },
+    handleDblClick: handleDblClick,
     handleLiClick: (e, theIndex) => {
       stepBackInTimeTo(theIndex)
     },
@@ -702,9 +541,10 @@ export default function Calculator({ domID = "my-calculator" }) {
   // console.log("1: [DONE] Push this repo to github! [DONE]")
   // console.log("2: [DONE] Display contents of appState.memory store. [DONE]")
   // console.log("3: [DONE] Display calculation sequence as-u-go. [DONE]")
-  // console.log("4: Implement the Undo/history feature.")
-  // console.log("5: Bug: When deleting 0 from display.")
-  // console.log("6: Bug: Rounding errors ???.")
+  // console.log("4: [DONE] Implement the Undo/history feature. X2 [DONE] ")
+  // console.log("5: [DONE] Bug: When deleting 0 from display. Removed keyboard Input")
+  // console.log("5: [DONE] Cont: functionality The unicode characters are not on a standard keyboard.")
+  // console.log("6: Bug: Rounding errors ??? Add a config option.")
   // console.log("7: ADD: Config menu & minimise button. Dragable!?")
   // console.log("8: Start a Basic To-do list component.")
 
@@ -729,11 +569,11 @@ export default function Calculator({ domID = "my-calculator" }) {
       </header>
 
       <Display
+        refs={displayRefs}
         curDisplayValue={appState.displayValue}
         curMemoryValue={appState.memory}
         curCalculations={appState.getCalculation()}
         theHistory={history}
-        isEditable={true}
         eventHandlers={displayEventHandlers}
       />
 
@@ -752,6 +592,7 @@ export default function Calculator({ domID = "my-calculator" }) {
 }
 
 const Display = ({
+  refs,
   curDisplayValue,
   curMemoryValue,
   curCalculations,
@@ -760,6 +601,9 @@ const Display = ({
   isEditable = false,
 }) => {
   const [show, setShow] = useState(false)
+  const menuRef = useRef(null)
+  const [primaryRef, memoryRef, calculationRef] = refs
+
   const handleCalculationClick = (e) => {
     setShow(!show)
   }
@@ -767,25 +611,44 @@ const Display = ({
     setShow(!show)
     eventHandlers.handleLiClick(e, theIndex)
   }
+  // Close menu if user clicks outside the menu when it's open
+  useEffect(() => {
+    const globalClick = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setShow(false)
+      }
+    }
+    document.addEventListener("mousedown", globalClick)
+    return () => document.removeEventListener("mousedown", globalClick)
+  })
+
   return (
     <div className="display-container">
       <div
+        ref={primaryRef}
         className="primary display"
-        onInput={eventHandlers.handleInput}
-        onKeyDown={eventHandlers.handleKeyDown}
+        onDoubleClick={eventHandlers.handleDblClick}
         contentEditable={isEditable}
         suppressContentEditableWarning={true}>
         {curDisplayValue}
       </div>
       <div className="sub-display-container">
-        <div className="memory sub display">{curMemoryValue}</div>
+        <div
+          ref={memoryRef}
+          className="memory sub display"
+          onDoubleClick={eventHandlers.handleDblClick}>
+          {curMemoryValue}
+        </div>
         <div className="csdc calculation-sub-display-container">
           <div
+            ref={calculationRef}
             className="calculation sub display"
+            onDoubleClick={eventHandlers.handleDblClick}
             onClick={handleCalculationClick}>
             {curCalculations}
           </div>
           <div
+            ref={menuRef}
             className={
               theHistory.length && show ? "history-list" : "history-list hide"
             }>
