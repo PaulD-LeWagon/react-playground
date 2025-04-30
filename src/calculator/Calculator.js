0c173b5b898963ea98033e4774dc222b11437fce
Paul Devanney <pauldevanney92@gmail.com>
2025-04-30 11:30:11 +0100

    Refactor state object

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -46,33 +46,144 @@ import "./Calculator.css"
 //   }
 // }
 
-export default function Calculator() {
-  // App state vars
-  const initState = {
-    displayValue: "0",
-    calcStack: [],
-    memory: "0",
+function injectDynamicVars(that, theObject = {}) {
+  for (const [k, v] of Object.entries(theObject)) {
+    that[k] = v
   }
-  const [appState, setAppState] = useState(initState)
-  const [history, setHistory] = useState([appState])
-  const [historyIndex, setHistoryIndex] = useState(0)
 
-  const getCurrentCalculation = () => {
-    return `${appState.calcStack.join("")}${appState.displayValue}`
+  // let k = "value"
+  // let i = 0
+  // for (i = 1; i < 3; i++) {
+  //   eval("let " + k + i + "= " + i + ";")
+  // }
+  // // eslint-disable-next-line no-undef
+  // console.log("value1=" + value1)
+  // // eslint-disable-next-line no-undef
+  // console.log("value2=" + value2)
+  // // eslint-disable-next-line no-undef
+  // console.log("value3=" + value3)
+
+  return that
+}
+
+// ;((x) => {
+//   let i
+//   // Window method
+//   for (i = 1; i < 5; i++) {
+//     window["value" + i] = +i
+//   }
+//   // eslint-disable-next-line no-undef
+//   console.log("value1=" + value1)
+//   // eslint-disable-next-line no-undef
+//   console.log("value2=" + value2)
+//   // eslint-disable-next-line no-undef
+//   console.log("value3=" + value3)
+
+//   // Create a new Map to store dynamic variables
+//   let dynamicMap = new Map()
+//   dynamicMap.set("memory", "0")
+//   dynamicMap.set("displayValue", "0")
+//   dynamicMap.set("calcStack", [])
+
+//   console.log(dynamicMap.get("memory"))
+//   console.log(dynamicMap.get("displayValue"))
+//   console.log(dynamicMap.get("calcStack"))
+
+//   // Generate dynamic variable names and assign values
+//   // for (let i = 1; i <= 4; i++) {
+//   //     dynamicMap.set(`value${i}`, i);
+//   // }
+
+//   // Access and print the dynamically created variables
+//   // dynamicMap.forEach((value, key) => {
+//   //     console.log(`${key} = ${value}`);
+//   // });
+
+//   console.log(x)
+// })("Hello Closure World!")
+
+export default function Calculator({ domID = "my-calculator" }) {
+  // App state vars
+  class AppState {
+    constructor(args = { memory: "0", displayValue: "0", calcStack: [] }) {
+      this.memory = args.memory
+      this.displayValue = args.displayValue
+      this.calcStack = args.calcStack
+    }
+    getCalculation() {
+      return `${this.calcStack.join("")}${this.displayValue}`
+    }
+    toString() {
+      return `m: (${this.memory}), dv: (${
+        this.displayValue
+      }) cs: [${this.calcStack.join(", ")}]`
+    }
+    newState(newState = {}) {
+      return Object.assign(new this.constructor(), this, newState)
+    }
+    *[Symbol.iterator]() {
+      yield* Object.values(this) // yield* Object.entries(this)
+    }
   }
+
+  const [appState, setAppState] = useState(new AppState())
+  const [history, setHistory] = useState([])
+  const [precision, setPrecision] = useState(3)
+
+  // console.log(appState.toString())
+
+  // function Test(one) {
+  //   this.one = one
+  // }
+  // const test = injectDynamicVars(new Test("Testing"), appState)
+  // console.log(test)
+
+  // console.log(
+  //   appState.newState({
+  //     memory: 27,
+  //     displayValue: 9,
+  //     calcStack: ["19", "-", "76", "-"],
+  //   })
+  // )
+
+  // function Test(one, two, ...allTheRest) {
+  //   this.one = one
+  //   this.two = two
+  //   this.allTheRest = allTheRest
+  // }
+  // Test.prototype.toString = function (printToConsole = false) {
+  //   let retString = ""
+  //   for (var [key, val] of Object.entries(this)) {
+  //     retString += `${key}: ${val}\n`
+  //   }
+  //   if (printToConsole) {
+  //     console.log(retString)
+  //   }
+  //   return retString
+  // }
+  // Test.prototype[Symbol.iterator] = function* () {
+  //   // yield* Object.entries(this)
+  //   yield this.one
+  //   yield this.two
+  //   yield this.allTheRest
+  // }
+  // var test = new Test(1, 2, 3, 'four', 'five', 'six', 'seven')
+  // console.log('Test: ', ...test)
+
+  // console.log(history)
 
   const stepBackInTimeTo = (thisIndex) => {
-    if (thisIndex >= 0 ) {
-      setAppState((as) => history[thisIndex])
-      setHistory(history.slice(0, thisIndex))
-    } else {
-      let newHistory = null
-      setAppState((as) => history.slice(-1)[0])
-      newHistory = history.slice(0, -1)
-      setHistory(newHistory)
-      thisIndex = newHistory.length -1
+    if (history.length) {
+      if (thisIndex >= 0) {
+        setAppState((as) => history[thisIndex])
+        setHistory(history.slice(0, thisIndex))
+      } else {
+        let newHistory = null
+        setAppState((as) => history.slice(-1)[0])
+        newHistory = history.slice(0, -1)
+        setHistory(newHistory)
+      }
     }
-    setHistoryIndex(thisIndex)
   }
 
   const addCurrentStateToHistory = () => {
@@ -85,14 +196,22 @@ export default function Calculator() {
     addCurrentStateToHistory()
     // Now we can update the appState
     setAppState((as) => {
-      return {
-        ...as,
-        // Just add it and the current value to the calcStack
+      // Just add displayValue and the theOperator to the calculation stack
+      // And empty the display ready for the next term
+      return appState.newState({
         calcStack: [...as.calcStack, as.displayValue, theOperator],
-        // The reset the appState.displayValue
         displayValue: "",
-      }
+      })
     })
+    // setAppState((as) => {
+    //   // Unpack the current state object
+    //   let [memory, displayValue, calcStack] = as
+    //   // Then just add theOperator and the displayValue to the calculation stack
+    //   calcStack = [...calcStack, displayValue, theOperator]
+    //   // And empty the display ready for the next term
+    //   displayValue = ""
+    //   return new AppState(memory, displayValue, calcStack)
+    // })
   }
 
   const doCommand = (theCommand) => {
@@ -108,14 +227,17 @@ export default function Calculator() {
         addCurrentStateToHistory()
         // Reset the appState values
         setAppState((as) => {
-          return {
-            // We don't reset the memory value
-            ...as,
-            // But we do reset everything else
+          return appState.newState({
             calcStack: [],
             displayValue: "0",
-          }
+          })
         })
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   displayValue = "0"
+        //   calcStack = []
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
         break
 
       case "=":
@@ -128,24 +250,24 @@ export default function Calculator() {
         addCurrentStateToHistory()
         // Do appState.memory recall - i.e. set appState.displayValue
         // to appState.memory variable.
-        setAppState((as) => {
-          return {
-            ...as,
-            displayValue: as.memory,
-          }
-        })
+        setAppState((as) => appState.newState({ displayValue: as.memory }))
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   displayValue = memory
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
         break
 
       case "M-":
         // Add current state to history variable
         addCurrentStateToHistory()
         // Do appState.memory delete - i.e. reset the appState.memory variable.
-        setAppState((as) => {
-          return {
-            ...as,
-            memory: "0",
-          }
-        })
+        setAppState((as) => appState.newState({ memory: "0" }))
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   memory = "0"
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
         break
 
       case "M+":
@@ -154,12 +276,17 @@ export default function Calculator() {
         // Do appState.memory add - i.e. add the appState.displayValue to appState.memory
         // variable (add to existing value if exists)
         setAppState((as) => {
-          return {
-            ...as,
+          return appState.newState({
             memory: String(Number(as.memory) + Number(as.displayValue)),
             displayValue: "0",
-          }
+          })
         })
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   memory = String(Number(memory) + Number(displayValue))
+        //   displayValue = "0"
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
         break
 
       case "％":
@@ -167,14 +294,21 @@ export default function Calculator() {
         addCurrentStateToHistory()
         // Do percent - i.e. take the input value and convert it to a percentage
         setAppState((as) => {
-          return {
-            ...as,
+          return appState.newState({
             displayValue:
               Number(as.displayValue) === 0
                 ? "0"
                 : String(Number(as.displayValue) / 100),
-          }
+          })
         })
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   displayValue =
+        //     Number(as.displayValue) === 0
+        //       ? "0"
+        //       : String(Number(as.displayValue) / 100)
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
         break
 
       default:
@@ -226,12 +360,17 @@ export default function Calculator() {
     // Add current state to history variable
     addCurrentStateToHistory()
     setAppState((as) => {
-      return {
-        ...as,
+      return appState.newState({
+        displayValue: String(Number(total.toFixed(precision))),
         calcStack: [],
-        displayValue: String(Number(total.toFixed(2))),
-      }
+      })
     })
+    // setAppState((as) => {
+    //   let [memory, displayValue, calcStack] = as
+    //   displayValue = String(Number(total.toFixed(precision)))
+    //   calcStack = []
+    //   return new AppState(memory, displayValue, calcStack)
+    // })
   }
 
   const handleOperatorButtonClick = (event, theValue) => {
@@ -251,51 +390,56 @@ export default function Calculator() {
     theValue = typeof theValue !== "string" ? String(theValue) : theValue
     if (appState.displayValue.length === 1 && appState.displayValue === "0") {
       if (theValue === "∙") {
-        setAppState((as) => {
-          return {
-            ...as,
-            displayValue: "0.",
-          }
-        })
+        setAppState((as) => appState.newState({ displayValue: "0." }))
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   displayValue = "0."
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
       } else {
-        setAppState((as) => {
-          return {
-            ...as,
-            displayValue: theValue,
-          }
-        })
+        setAppState((as) => appState.newState({ displayValue: theValue }))
+        // setAppState((as) => {
+        //   let [memory, displayValue, calcStack] = as
+        //   displayValue = theValue
+        //   return new AppState(memory, displayValue, calcStack)
+        // })
       }
     } else {
+      theValue = theValue !== "∙" ? theValue : "."
       setAppState((as) => {
-        return {
-          ...as,
+        return appState.newState({
           displayValue: `${as.displayValue}${theValue}`,
-        }
+        })
       })
+      // setAppState((as) => {
+      //   let [memory, displayValue, calcStack] = as
+      //   displayValue = `${displayValue}${theValue}`
+      //   return new AppState(memory, displayValue, calcStack)
+      // })
     }
   }
 
   const handleInputChange = (event, theValue) => {
     console.debug("InputChange: ", theValue)
-    const opsMatch = theValue.trim().match(/[*/+-^]|RT+$/)
-    if (opsMatch) {
-      doOperation(opsMatch.shift())
-    } else {
-      const comMatch = theValue.trim().match(/[C=%]|M\+|M-|M+$/)
-      if (comMatch) {
-        doCommand(comMatch.shift())
-      } else {
-        // Add current state to history variable
-        addCurrentStateToHistory()
-        // Strip any illegal characters and update app state
-        setAppState((as) => {
-          return {
-            ...as,
-            displayValue: theValue.replace(/[^0-9.]+/, ""),
-          }
-        })
-      }
-    }
+    // const opsMatch = theValue.trim().match(/[*/+-^]|RT+$/)
+    // if (opsMatch) {
+    //   doOperation(opsMatch.shift())
+    // } else {
+    //   const comMatch = theValue.trim().match(/[C=%]|M\+|M-|M+$/)
+    //   if (comMatch) {
+    //     doCommand(comMatch.shift())
+    //   } else {
+    //     // Add current state to history variable
+    //     addCurrentStateToHistory()
+    //     // Strip any illegal characters and update app state
+    //     setAppState((as) => appState.newState({ displayValue: theValue.replace(/[^0-9.]+/, "") }))
+    //    // setAppState((as) => {
+    //    //   let [memory, displayValue, calcStack] = as
+    //    //   displayValue = theValue.replace(/[^0-9.]+/, "")
+    //    //   return new AppState(memory, displayValue, calcStack)
+    //    // })
+    //   }
+    // }
   }
 
   const displayEventHandlers = {
@@ -307,10 +451,13 @@ export default function Calculator() {
         doCommand("=")
       }
     },
+    handleLiClick: (e, theIndex) => {
+      stepBackInTimeTo(theIndex)
+    },
   }
 
   const btnType = "button"
-  const stdBtnClass = "calc-btn"
+  const stdBtnClass = "btn"
   const calcButtons = [
     {
       btnType: btnType,
@@ -530,6 +677,27 @@ export default function Calculator() {
     },
   ]
 
+  const configButtons = [
+    {
+      btnType: btnType,
+      className: stdBtnClass,
+      title: "Close",
+      children: "X",
+      clickHandler: (e) => {
+        handleCommandButtonClick(e, "XX")
+      },
+    },
+    {
+      btnType: btnType,
+      className: stdBtnClass,
+      title: "Set precision e.g. 2 d.p. [0.00]",
+      children: "D",
+      clickHandler: (e) => {
+        handleCommandButtonClick(e, "DP")
+      },
+    },
+  ]
+
   // console.log("To-do:")
   // console.log("1: [DONE] Push this repo to github! [DONE]")
   // console.log("2: [DONE] Display contents of appState.memory store. [DONE]")
@@ -541,22 +709,35 @@ export default function Calculator() {
   // console.log("8: Start a Basic To-do list component.")
 
   return (
-    <div className="calculator">
-      <header className="calc-header">
-        <h4 className="calc-title">Basic Calculator</h4>
-        <h5 className="calc-sub-title">Version 1.0</h5>
+    <div
+      id={domID}
+      className="calculator">
+      <div className="config-panel hide">
+        {configButtons.map((btnData, i) => {
+          return (
+            <Button
+              key={i}
+              {...btnData}
+            />
+          )
+        })}
+      </div>
+
+      <header className="header">
+        <h4 className="title">Basic Calculator</h4>
+        <h5 className="sub-title">Version 1.0</h5>
       </header>
 
       <Display
         curDisplayValue={appState.displayValue}
         curMemoryValue={appState.memory}
-        curCalculations={getCurrentCalculation()}
+        curCalculations={appState.getCalculation()}
         theHistory={history}
         isEditable={true}
-        {...displayEventHandlers}
+        eventHandlers={displayEventHandlers}
       />
 
-      <div className="calc-keypad">
+      <div className="keypad">
         {calcButtons.map((btnData, i) => {
           return (
             <Button
@@ -574,40 +755,52 @@ const Display = ({
   curDisplayValue,
   curMemoryValue,
   curCalculations,
-  handleInput,
-  handleKeyDown,
+  eventHandlers,
   theHistory,
   isEditable = false,
 }) => {
   const [show, setShow] = useState(false)
   const handleCalculationClick = (e) => {
     setShow(!show)
-    console.log("Implement history list...")
+  }
+  const handleCalcLiClick = (e, theIndex) => {
+    setShow(!show)
+    eventHandlers.handleLiClick(e, theIndex)
   }
   return (
-    <div className="calc-display-container">
+    <div className="display-container">
       <div
-        className="calc-primary display"
-        onInput={handleInput}
-        onKeyDown={handleKeyDown}
+        className="primary display"
+        onInput={eventHandlers.handleInput}
+        onKeyDown={eventHandlers.handleKeyDown}
         contentEditable={isEditable}
         suppressContentEditableWarning={true}>
         {curDisplayValue}
       </div>
-      <div className="calc-sub-display-container">
-        <div className="calc-memory sub display">{curMemoryValue}</div>
-        <div className="calc-calculation-sub-display-wrapper">
+      <div className="sub-display-container">
+        <div className="memory sub display">{curMemoryValue}</div>
+        <div className="csdc calculation-sub-display-container">
           <div
-            className="calc-calculation sub display"
+            className="calculation sub display"
             onClick={handleCalculationClick}>
             {curCalculations}
           </div>
           <div
-            className={show ? "calc-history-list" : "calc-history-list hide"}>
+            className={
+              theHistory.length && show ? "history-list" : "history-list hide"
+            }>
             <ul>
-              <li>Version 1</li>
-              {theHistory.map((ss, i)=>{
-                return <li key={i} onClick={(e)=>{console.log("li", i)}}>Snapshot: {i + 1} - {ss.calcStack.join('') + ss.displayValue}</li>
+              {theHistory.map((ss, i) => {
+                return (
+                  <li
+                    key={i}
+                    onClick={(e) => {
+                      handleCalcLiClick(e, i)
+                    }}>
+                    <span>Snapshot: {i + 1}</span>
+                    <span>{ss.calcStack.join("") + ss.displayValue}</span>
+                  </li>
+                )
               })}
             </ul>
           </div>
